38289e344091239f1ef9ae7bae4d529925d4faff
vince-CV <vince@arylla.com>
2019-11-22 11:40:25 -0500

    py scripts

---
+++ reformdata/ios_rotation.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+from PIL import Image
+
+path = "C:/Users/xwen2/Desktop/YOLOv3/VOCdevkit/VOC2019/images/train2014/"
+imagelist = os.listdir(path)
+
+for image_name in imagelist:
+
+    print(image_name)
+    image = Image.open(path+image_name)
+
+    if hasattr(image, '_getexif'):
+        dict_exif = image._getexif()
+        print(dict_exif[274])
+
+        if dict_exif[274] == 3:
+            new_img = image.rotate(-90)
+        elif dict_exif[274] == 6:
+            new_img = image.rotate(-90,0,1)
+        else:
+            new_img = image
+        
+    else:
+        new_img = image
+
+    new_img.save('C:/Users/xwen2/Desktop/YOLOv3/VOCdevkit/VOC2019/images'+image_name)
