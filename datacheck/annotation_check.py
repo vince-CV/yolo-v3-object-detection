6fd82a95f2f7f1569ea7fec1e369289fb8d0eac5
vince-CV <vince@arylla.com>
2020-08-27 17:27:58 -0400

    add some details

--- datacheck/annotation_check.py
+++ datacheck/annotation_check.py
@@ -3,40 +3,63 @@ import os
 import numpy as np
 from PIL import Image
 
-image_path = "C:/Users/xwen2/Desktop/YOLOv3/VOCdevkit/VOC2019/images/train2014/"
-anota_path = "C:/Users/xwen2/Desktop/YOLOv3/VOCdevkit/VOC2019/labels/"
+image_path = "C:/Users/xwen2/Google Drive/Mask Detector/data/"
+anota_path = "C:/Users/xwen2/Google Drive/Mask Detector/data/"
 
 
 imagelist = os.listdir(image_path)
                      
 for image_name in imagelist:
-    (nameWithoutExtention, extention) = os.path.splitext(os.path.basename(image_name))
-    print(image_path+image_name)
-    image = cv.imread(image_path+image_name)
+        
+        (nameWithoutExtention, extention) = os.path.splitext(os.path.basename(image_name))
 
-    yolo = np.loadtxt(anota_path+nameWithoutExtention+".txt")
+        if (extention != ".txt"): 
+            print(image_path+image_name)
+            image = cv.imread(image_path+image_name)
 
-    image_size =image.shape
-    height = image_size[0]
-    width = image_size[1]
+            yolo = np.loadtxt(anota_path+nameWithoutExtention+".txt")
 
-    center_x = int(width*yolo[1])
-    center_y = int(height*yolo[2])
+            dim = np.array(yolo)
+            print("dim: ", dim.ndim)
 
-    box_width = int(width*yolo[3])
-    box_height = int(height*yolo[4])
+            number_of_box = len(yolo)
+            image_size =image.shape
+            height = image_size[0]
+            width = image_size[1]
 
-    x = int(center_x-0.5*box_width)
-    y = int(center_y-0.5*box_height)
 
-    X = int(center_x+0.5*box_width)
-    Y = int(center_y+0.5*box_height)
+            if (dim.ndim == 1):
+                center_x = int(width*yolo[1])
+                center_y = int(height*yolo[2])
+                box_width = int(width*yolo[3])
+                box_height = int(height*yolo[4])
+                x = int(center_x-0.5*box_width)
+                y = int(center_y-0.5*box_height)
+                X = int(center_x+0.5*box_width)
+                Y = int(center_y+0.5*box_height)
+                if (yolo[0] == 0):
+                    cv.rectangle(image,(x, y),(X, Y),(55,255,155),5)
+                else:
+                    cv.rectangle(image,(x, y),(X, Y),(55,55,255),5)
+            else:
+                for item in range(number_of_box):
+                    center_x = int(width*yolo[item][1])
+                    center_y = int(height*yolo[item][2])
+                    box_width = int(width*yolo[item][3])
+                    box_height = int(height*yolo[item][4])
+                    x = int(center_x-0.5*box_width)
+                    y = int(center_y-0.5*box_height)
+                    X = int(center_x+0.5*box_width)
+                    Y = int(center_y+0.5*box_height)
+                    if (yolo[item][0] == 0):
+                        cv.rectangle(image,(x, y),(X, Y),(55,255,155),5)
+                    else:
+                        cv.rectangle(image,(x, y),(X, Y),(55,55,255),5)
+            
+            image = cv.resize(image, (600, 600))
 
-    cv.rectangle(image,(x, y),(X, Y),(55,255,155),30)
-    image = cv.resize(image, (600, 600))
-
-    cv.imshow("annatation", image)
-    cv.waitKey()
+            cv.imshow("annatation", image)
+            cv.waitKey()
 
     
 
